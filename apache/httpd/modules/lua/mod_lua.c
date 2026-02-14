e6c1496ff7e277fdc6da964d6c41100d25a54c57
pr-hung <bkedx0000@gmail.com>
2026-02-15 06:04:28 +0800

    Fix potential vulnerability in cloned code (apache/httpd/modules/lua/mod_lua.c)

--- apache/httpd/modules/lua/mod_lua.c
+++ apache/httpd/modules/lua/mod_lua.c
@@ -1721,6 +1721,7 @@ static const char *lua_authz_parse(cmd_parms *cmd, const char *require_line,
     const char *provider_name;
     lua_authz_provider_spec *spec;
     lua_authz_provider_func *func = apr_pcalloc(cmd->pool, sizeof(lua_authz_provider_func));
+    lua_authz_provider_func *func = apr_pcalloc(cmd->pool, sizeof(lua_authz_provider_func));
 
     apr_pool_userdata_get((void**)&provider_name, AUTHZ_PROVIDER_NAME_NOTE,
                           cmd->temp_pool);
